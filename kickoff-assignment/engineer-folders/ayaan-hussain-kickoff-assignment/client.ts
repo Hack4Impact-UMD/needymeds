5eff80d40ba7c79a9bf04f34695394a7ca1579db
Ayaan Hussain <azh13@terpmail.umd.edu>
2025-09-23 19:45:27 -0400

    Completed Engineer Onboarding Assignment

---
+++ kickoff-assignment/engineer-folders/ayaan-hussain-kickoff-assignment/client.ts
@@ -0,0 +1,11 @@
+export interface Joke {
+    joke: string
+}
+
+export async function getJoke(): Promise<Joke> {
+    const url = "https://v2.jokeapi.dev/joke/Any?type=single";
+    const response = await fetch(url);
+  
+    const result = await response.json();
+    return { joke: result.joke }
+}
