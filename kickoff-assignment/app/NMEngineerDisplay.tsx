90ca84f270860bf520a90c016a8a869f4000fe3f
om-arya <om.arya0577@gmail.com>
2025-09-24 13:51:59 -0400

    Re-add missing start tag

--- kickoff-assignment/app/NMEngineerDisplay.tsx
+++ kickoff-assignment/app/NMEngineerDisplay.tsx
@@ -1,15 +1,13 @@
-import React from "react";
-import { ScrollView } from "react-native";
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import React from 'react';
+import { ScrollView } from 'react-native';
 
-import NMEngineer from "../components/NMEngineer";
+import NMEngineer from '../components/NMEngineer';
 
 export default function NMEngineerDisplay() {
   return (
     <>
-      <Stack.Screen
-        options={{ title: "🏆 NeedyMeds Engineer Display 🏆" }}
-      />
+      <Stack.Screen options={{ title: '🏆 NeedyMeds Engineer Display 🏆' }} />
       <ScrollView>
         <NMEngineer
           name="Om Arya"
@@ -18,7 +16,7 @@ export default function NMEngineerDisplay() {
           linkedin="https://www.linkedin.com/in/om-arya"
           github="https://github.com/om-arya"
           email="om.arya0577@gmail.com"
-          profilePictureURL={require("../assets/images/om-arya.jpg")}
+          profilePictureURL={require('../assets/images/om-arya.jpg')}
         />
         <NMEngineer
           name="Samarth Kolanupaka"
@@ -27,7 +25,7 @@ export default function NMEngineerDisplay() {
           linkedin="https://www.linkedin.com/in/samarth-kolanupaka-0b2019259/"
           github="https://github.com/samarth212"
           email="samarth212@gmail.com"
-          profilePictureURL={require("../assets/images/samarth-kolanupaka.jpg")}
+          profilePictureURL={require('../assets/images/samarth-kolanupaka.jpg')}
         />
 
         <NMEngineer
@@ -37,17 +35,17 @@ export default function NMEngineerDisplay() {
           linkedin="https://www.linkedin.com/in/bhavya-tanugula-3650b3306/"
           github="https://github.com/bhavyat-01"
           email="bhavya.tanugula@gmail.com"
-          profilePictureURL={require("../assets/images/bhavya-tanugula.jpg")}
+          profilePictureURL={require('../assets/images/bhavya-tanugula.jpg')}
         />
 
-        <NMEngineer 
+        <NMEngineer
           name="Ayaan Hussain"
           year="Senior"
           position="Engineer"
           linkedin="https://www.linkedin.com/in/ayaan-z-hussain/"
           github="https://github.com/ayaanh-03"
           email="ayaanzh13@gmail.com"
-          profilePictureURL={require("../assets/images/ayaan-hussain.jpg")}
+          profilePictureURL={require('../assets/images/ayaan-hussain.jpg')}
         />
 
         <NMEngineer
@@ -57,16 +55,17 @@ export default function NMEngineerDisplay() {
           linkedin="https://www.linkedin.com/in/ngo-angela/"
           github="https://github.com/ango06"
           email="ngoangela6@gmail.com"
-          profilePictureURL={require("../assets/images/angela-ngo.jpg")}
+          profilePictureURL={require('../assets/images/angela-ngo.jpg')}
         />
-        
+
+        <NMEngineer
           name="Parsa Sedghi"
           year="Senior"
           position="Engineer"
           linkedin="https://linkedin.com/in/parsa-sedghi"
           github="https://github.com/psedghi"
           email="psedghi@umd.edu"
-          profilePictureURL={require("../assets/images/parsa-sedghi.jpg")}
+          profilePictureURL={require('../assets/images/parsa-sedghi.jpg')}
         />
 
         <NMEngineer
@@ -76,7 +75,7 @@ export default function NMEngineerDisplay() {
           linkedin="https://www.linkedin.com/in/dhanyadesai/"
           github="https://github.com/dhanyades"
           email="dhanya.desai@gmail.com"
-          profilePictureURL={require("../assets/images/dhanya-desai.jpg")}
+          profilePictureURL={require('../assets/images/dhanya-desai.jpg')}
         />
 
         <NMEngineer
@@ -86,7 +85,7 @@ export default function NMEngineerDisplay() {
           linkedin="https://www.linkedin.com/in/eileendchen/"
           github="https://github.com/eileendchen"
           email="eileendchen@gmail.com"
-          profilePictureURL={require("../assets/images/eileen-chen.jpg")}
+          profilePictureURL={require('../assets/images/eileen-chen.jpg')}
         />
       </ScrollView>
     </>
