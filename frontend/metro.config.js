83a5dc954b583f23013b5826f9307bbde234d40c
Annie Nguyen <lunagod4421@gmail.com>
2026-03-05 17:19:15 -0500

    fix: added wasm to fix white screen on web

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -4,6 +4,7 @@ const config = getDefaultConfig(__dirname);
 
 // Allow SQLite .db files to be bundled
 config.resolver.assetExts.push('db');
+config.resolver.assetExts.push('wasm');
 
 // Remove 'db' from sourceExts if it was added there
 config.resolver.sourceExts = config.resolver.sourceExts.filter((ext) => ext !== 'db');
