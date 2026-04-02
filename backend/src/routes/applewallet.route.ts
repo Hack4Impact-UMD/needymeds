ff466f1b9056317a88a3925b8ccfcb8244e8b2f4
Bhavya Tanugula <bhavya.tanugula@gmail.com>
2026-04-02 18:08:53 -0400

    Changed button logic

--- backend/src/routes/applewallet.route.ts
+++ backend/src/routes/applewallet.route.ts
@@ -1,10 +1,13 @@
+console.log('applewallet.route.ts loaded');
 import { Router } from 'express';
 import { createPass } from '../services/applewallet.service';
 
 const router = Router();
 
-router.get('/', async (req, res, next) => {
+router.post('/', async (req, res, next) => {
+  console.log('Route hit');
   try {
+    console.log('About to call createPass');
     const cardNumber = '90MA019309343023';
 
     const pkpass = await createPass({
@@ -16,8 +19,14 @@ router.get('/', async (req, res, next) => {
     res.setHeader('Content-Disposition', 'attachment; filename=needymeds.pkpass');
 
     res.status(200).end(pkpass);
-  } catch (error) {
-    next(error);
+  } catch (error: any) {
+    console.error('Apple Wallet route error:', error.message); // shows error message
+    console.error(error.stack); // shows full stack trace
+    res.status(500).json({
+      message: 'Internal Server Error',
+      details: error.message,
+      stack: error.stack,
+    });
   }
 });
 
--- backend/src/services/applewallet.service.ts
+++ backend/src/services/applewallet.service.ts
@@ -1,3 +1,4 @@
+console.log('applewallet.service.ts loaded');
 import { PKPass } from 'passkit-generator';
 import path from 'path';
 import { getAppleWalletSecret, getAppleWalletWWDRSecret } from '../secrets/secrets';
@@ -9,6 +10,7 @@ type PassInput = {
 
 export async function createPass({ serial, number }: PassInput) {
   /*Gets the secret from AWS*/
+  console.log('Reached the Create Pass function');
   const secret = await getAppleWalletSecret();
   console.log('Apple Wallet certificate length:', secret.signerCert.length);
 
@@ -17,7 +19,7 @@ export async function createPass({ serial, number }: PassInput) {
 
   const pass = await PKPass.from(
     {
-      model: path.join(__dirname, '../wallet-pass'),
+      model: path.join(process.cwd(), 'wallet-pass.pass'),
       certificates: {
         signerCert: secret.signerCert,
         signerKey: secret.signerKey,
